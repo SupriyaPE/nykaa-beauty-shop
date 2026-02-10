ef9885e1a4e7ef5ad5a756673c19a66c14a631b8
SupriyaPE <supriyape2002@gmail.com>
2026-02-10 11:49:18 +0530

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,35 +1,7 @@
 #!/bin/sh
+echo "🚀 Starting server..."
 
-echo "⏳ Waiting for database..."
+python manage.py migrate --noinput || echo "⚠️ migrate skipped"
+python manage.py collectstatic --noinput || echo "⚠️ collectstatic skipped"
 
-python << END
-import time
-import sys
-import MySQLdb
-
-host = "${DB_HOST}"
-port = int("${DB_PORT}")
-
-for i in range(30):
-    try:
-        db = MySQLdb.connect(host=host, port=port, user="${DB_USER}", passwd="${DB_PASSWORD}")
-        db.close()
-        print("✅ Database is ready")
-        sys.exit(0)
-    except Exception as e:
-        print("DB not ready, retrying...")
-        time.sleep(2)
-
-print("❌ Could not connect to DB")
-sys.exit(1)
-END
-
-
-echo "🚀 Running migrations..."
-python manage.py migrate --noinput
-
-echo "📦 Collecting static..."
-python manage.py collectstatic --noinput
-
-echo "🎯 Starting server..."
 exec "$@"
